d1ef89ad67c6c7f98fd1402c441c9c5443dddea7
Em <ekramm@me.com>
2022-07-11 18:53:09 -0400

    created a login page

---
+++ client/src/components/Login.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+
+function Login({onLogin}) {
+  const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch('/login', {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username }),
+        })
+        .then((r) => r.json())
+        .then((user) => onLogin(user))
+      }
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username: </label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+    <label htmlFor="password">Password: </label>
+      <input
+        type="text"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  )
+}
+
+
+export default Login
